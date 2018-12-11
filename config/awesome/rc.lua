0e2d3cdd1a48c651f2c5cb33208decbddbc94e5f
liufengfan <liufengfan@163.com>
2018-12-11 20:15:14 +0800

    add Xresources

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -44,8 +44,9 @@ end
 beautiful.init(os.getenv("HOME") .. "/.config/awesome/themes/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "gnome-terminal"
-editor = os.getenv("EDITOR") or "nano"
+-- terminal = "gnome-terminal"
+terminal = "urxvt"
+editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
