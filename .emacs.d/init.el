afe32875336a4cedaa1577fb11cdc5ee847ea82e
liuff1 <1150216742@qq.com>
2018-12-11 20:59:25 +0800

    update resources

---
+++ .emacs.d/init.el
@@ -0,0 +1,12 @@
+;; close welcome page
+(setq inhibit-splash-screen t)
+;; set tool bar nil
+(tool-bar-mode -1)
+;; set scroll bar nil
+(scroll-bar-mode -1)
+;; modify the cursor
+(setq-default cursor-type 'bar)
+;; highlight current line
+;; (global-hl-line-mode t)
+;; set line number
+(menu-bar-mode -1)
