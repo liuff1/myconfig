e189f8639e841ac8ae085318001c5b4700720fc7
liufengfan <41888267+liuff1@users.noreply.github.com>
2018-12-13 15:48:47 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -s $HOME/.myconfig/.vimrc $HOME/.vimrc
+ln -s $HOME/.myconfig/.Xresources $HOME/.Xresources
+ln -s $HOME/.myconfig/.zshrc $HOME/.zshrc
+ln -s $HOME/.myconfig/.conkyrc $HOME/.conkyrc
+ln -s $HOME/.myconfig/.emacs.d $HOME/.emacs.d
+ln -s $HOME/.myconfig/config/awesome $HOME/.config/awesome
+ln -s $HOME/.myconfig/config/i3 $HOME/.config/i3
