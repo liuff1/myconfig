ae25e53b2b374de49febb7cbac54ad0a4a0ac6fd
liufengfan <41888267+liuff1@users.noreply.github.com>
2018-12-19 17:37:05 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,10 +8,38 @@ if [ ! -f ~/.vim/autoload/plug.vim ]; then
 fi
 
 
+if [ -f ~/.vimrc ]; then
+    mv ~/.vimrc ~/.vimrc.bak
+fi
 ln -s $HOME/.myconfig/.vimrc $HOME/.vimrc
+
+if [ -f ~/.Xresources ]; then
+    mv ~/.Xresources ~/.Xresources.bak
+fi
 ln -s $HOME/.myconfig/.Xresources $HOME/.Xresources
+
+if [ -f ~/.zshrc ]; then
+    mv ~/.zshrc ~/.zshrc.bak
+fi
 ln -s $HOME/.myconfig/.zshrc $HOME/.zshrc
+
+if [ -f ~/.conkyrc ]; then
+    mv ~/.conkyrc ~/.conkyrc.bak
+fi
 ln -s $HOME/.myconfig/.conkyrc $HOME/.conkyrc
+
+if [ -d ~/.emacs.d/ ]; then
+    mv ~/.emacs.d ~/.emacs.d.bak
+fi
 ln -s $HOME/.myconfig/.emacs.d $HOME/.emacs.d
+
+if [ -d ~/.config/awesome/ ]; then
+    mv ~/.config/awesome ~/.config/awesome.bak
+fi
 ln -s $HOME/.myconfig/config/awesome $HOME/.config/awesome
+
+if [ -d ~/.config/i3/ ]; then
+    mv ~/.config/i3 ~/..config/i3.bak
+fi
 ln -s $HOME/.myconfig/config/i3 $HOME/.config/i3
+
